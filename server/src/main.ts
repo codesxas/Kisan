4ad38b38d6923755166436e05f6c9656290124d1
codesxas <39775875+codesxas@users.noreply.github.com>
2022-06-18 10:26:27 +0530

    adding to heroku

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,14 +1,15 @@
 import { configService } from "./config/config.service";
-import { AppModule } from "./modules/app.module";
+// import { AppModule } from "./modules/app.module";
 
-const port = configService.getPort();
+// const port = configService.getPort();
 
 async function server() {
-  const app = new AppModule().create();
+  console.log('first')
+  // const app = new AppModule().create();
 
-  await app.listen(port, () => {
-    console.log(`App is listening at http://localhost:${port}`);
-  });
+  // await app.listen(port, () => {
+  //   console.log(`App is listening at http://localhost:${port}`);
+  // });
 }
 
 server();
