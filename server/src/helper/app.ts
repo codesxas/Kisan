67a798181cdbce3da0c54f42c3f45273f167572c
codesxas <39775875+codesxas@users.noreply.github.com>
2022-06-21 22:55:15 +0530

    added test data

--- server/src/helper/app.ts
+++ server/src/helper/app.ts
@@ -1,9 +1,11 @@
 import cors from "cors";
 import express, { Express } from "express";
+import _ from "lodash";
 
 import { errorHandler } from "./error.helper";
 import { ContactController } from "../modules/contacts/controllers";
 import { HistoryController } from "../modules/history/controllers";
+import { ContactService } from "../modules/contacts/services";
 
 class App {
   app: Express;
@@ -22,6 +24,47 @@ class App {
     );
   }
 
+  public async createTestData() {
+    const seedId = Date.now()
+      .toString()
+      .split("")
+      .reverse()
+      .reduce((s, it, x) => (x > 3 ? s : (s += it)), "");
+
+    const statusArray = ["Available", "Unavailable"];
+    const organizationArray = [
+      "Zencorporation",
+      "Groovestreet",
+      "Iselectrics",
+      "Toughzap",
+      "Konmatfix",
+    ];
+
+    const service = new ContactService();
+
+    const data = _.range(1, 20)
+      .map((n) => {
+        return {
+          body: {
+            firstName: `FN${seedId}-${n}`,
+            lastName: `LN${seedId}-${n}`,
+            areaCode: "+91",
+            phoneNumber: `${7082537507 + n}`,
+            status: statusArray[Math.floor(Math.random() * statusArray.length)],
+            organization:
+              organizationArray[
+                Math.floor(Math.random() * organizationArray.length)
+              ],
+          },
+        };
+      })
+      .map((item) => {
+        service.postContact(item, null, null);
+      });
+
+    return await Promise.all(data);
+  }
+
   public create(): Express {
     this.app.use(cors());
     this.useParser();
@@ -34,6 +77,10 @@ class App {
 
     const history = new HistoryController();
     this.app.use(history.router);
+
+    // this.createTestData()
+    //   .then((_) => console.log("...wait for script to exit"))
+    //   .catch((error) => console.error("seed error", error));
 
     return this.app;
   }
